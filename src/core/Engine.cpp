67c77fcb36b1f440b7c6726d53c67dafc0f7aa71
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-06-02 12:17:15 -0600

    docs: documentation for Engine

--- src/core/Engine.cpp
+++ src/core/Engine.cpp
@@ -13,12 +13,13 @@ Engine::Engine(std::string tableroSource, std::vector<PlayerStats>& jugadores)
     , enemiesSystem_(enemigos_, explosiones_, tablero_, eventBus_)
     , bombsSystem_(jugadores_, bombas_, explosiones_, enemigos_, tablero_, eventBus_)
 {
+    //Mutex para inputs provenientes de inputHandler
     pthread_mutex_init(&inputMutex_, NULL);
 
     singlePlayer_ = (jugadores.size() == 1);
-
     const auto& playersSpawn = tablero_.getSpawnPlayers();
     
+    //La creacion de las entidades se hace en Engine, y se pasa despues al Tablero
     for (int i=0; i < jugadores.size(); i++)
     {
         const PlayerStats& info = jugadores.at(i);
@@ -35,27 +36,21 @@ Engine::Engine(std::string tableroSource, std::vector<PlayerStats>& jugadores)
         default: break;}
 
     };
-    const auto& enemySpawns = tablero_.getSpawnEnemies();
 
+    //Creacion de enemigos
+    const auto& enemySpawns = tablero_.getSpawnEnemies();
     int enemyId = 0;
     for (const Position& pos : enemySpawns)
     {
         enemigos_.push_back(Enemigo(enemyId, 1, 3, pos.x, pos.y));
-
-        tablero_.addOccupant(
-            pos,
-            Occupant{
-                EntityType::Enemy,
-                enemyId
-            }
-        );
-
+        tablero_.addOccupant(pos, Occupant{ EntityType::Enemy, enemyId });
         enemyId++;
     }
 }
 
 Engine::~Engine()
 {
+    //Se verifica si se usaron hilos en este Engine
     bool started = (state_ != MatchState::Preparing);
 
     state_ = MatchState::Finished;
@@ -69,6 +64,7 @@ Engine::~Engine()
     pthread_mutex_destroy(&inputMutex_);
 }
 
+//Creacion de hilos del Engine, se tiene por separado para que los tests no tengan que usar hilos
 void Engine::start()
 {
     if (state_ != MatchState::Preparing) return;
@@ -93,7 +89,7 @@ void Engine::update(sf::Time dt)
     updateGameState();
 }
 
-// logic_thread
+// Hilo de logica, se encarga de procesar eventos del eventQueue
 void* Engine::logic_thread(void* arg)
 {
     Engine* engine = static_cast<Engine*>(arg);
@@ -101,13 +97,13 @@ void* Engine::logic_thread(void* arg)
     while (engine -> state() == MatchState::Playing)
     {
         auto evento = engine -> popEvento();
-
         if (evento) engine -> procesarEvento(*evento);
     }
 
     return nullptr;
 }
 
+//Hilo de enemigos, se encarga de actualizar enemigos con un subsistema por aparte
 void* Engine::enemy_thread(void* arg)
 {
     Engine* engine = static_cast<Engine*>(arg);
@@ -137,10 +133,7 @@ std::optional<Evento> Engine::popEvento()
 void Engine::procesarEvento(Evento& evento){
     switch (evento.tipo())
     {
-        case EventType::PlayerMove:
-            onPlayerMove(evento);
-            break;
-            
+        case EventType::PlayerMove:         onPlayerMove(evento); break; 
         case EventType::EnemyMove:
             {
                 auto damaged = enemiesSystem_.processMove(evento);
@@ -148,38 +141,13 @@ void Engine::procesarEvento(Evento& evento){
                 if (damaged) danioPlayer(*damaged);
                 break;
             } 
-        
-        case EventType::PlayerPlaceBomb:
-            onPlayerPlaceBomb(evento);
-            break;
-
-        case EventType::BombExplode:
-            onBombExplode(evento);
-            break;
-
-        case EventType::PlayerDamage:
-            danioPlayer(evento.objetivo());
-            break;
-
-        case EventType::PlayerDeath:
-            //onPlayerDeath(evento);
-            break;
-
-        case EventType::TileDestroyed:
-            onTileDestroyed(evento);
-            break;
-        
-        case EventType::EnemyDeath:
-            onEnemyDeath(evento);
-            break;
-
-        case EventType::ChainExplosion:
-            onChainExplosion(evento);
-            break;
-
-        case EventType::PlayerPickPowerUp:
-            onPlayerPickPowerUp(evento);
-            break;
+        case EventType::PlayerPlaceBomb:    onPlayerPlaceBomb(evento); break;
+        case EventType::BombExplode:        onBombExplode(evento); break;
+        case EventType::PlayerDamage:       danioPlayer(evento.objetivo()); break;
+        case EventType::TileDestroyed:      onTileDestroyed(evento); break;       
+        case EventType::EnemyDeath:         onEnemyDeath(evento); break;
+        case EventType::ChainExplosion:     onChainExplosion(evento); break;
+        case EventType::PlayerPickPowerUp:  onPlayerPickPowerUp(evento); break;
     }
 }
 
@@ -203,6 +171,7 @@ void Engine::updateGameState()
 
     bool noEnemies = !enemiesSystem_.hasLivingEnemies();
 
+    //Se revisa si el juego ha terminado, en base al numero de jugadores vivos y el modo de juego
     if (singlePlayer_)
     {
         if (alivePlayers == 0) state_ = MatchState::WaitingForGameOverConfirmation;
@@ -217,16 +186,16 @@ void Engine::updateGameState()
 
 RenderSnapshot Engine::makeRenderSnapshot()
 {
+    //Se crea un snapshot desde 0, copiando los contenidos relevantes del juego para que sean renderizados
     RenderSnapshot snapshot;
 
     int width  = tablero_.getWidth();
     int height = tablero_.getHeight();
 
     snapshot.tiles.resize(height, std::vector<TileType>(width));
-
     snapshot.entities.resize(height, std::vector<EntityType>(width, EntityType::None));
 
-    // Copy board state
+    // Se recorre el tablero
     for (int y = 0; y < height; ++y)
     {
         for (int x = 0; x < width; ++x)
@@ -236,11 +205,12 @@ RenderSnapshot Engine::makeRenderSnapshot()
             const BoardCell& cell = tablero_.getCell(pos);
             snapshot.tiles[y][x] = cell.terrainType;
 
-            // Simplified:
-            // render first occupant only
+            //Solo se renderiza la primera entidad en la celda del tablero
             if (!cell.occupants.empty())
             {
                 for (Occupant occ : cell.occupants) {
+                    
+                    //La entidad de enemigo y de explosion tienen prioridad de renderizado 
                     if (occ.type == EntityType::Explosion)
                     {
                         snapshot.entities[y][x] = EntityType::Explosion;
@@ -262,7 +232,7 @@ RenderSnapshot Engine::makeRenderSnapshot()
         }
     }
 
-    // Copy player data
+    // Se copian los stats de los jugadores
     for (const Player& p : jugadores_)
     {
         snapshot.players.push_back(PlayerData{
@@ -276,7 +246,7 @@ RenderSnapshot Engine::makeRenderSnapshot()
         });
     }
 
-    // HUD data
+    // Datos del HUD
     snapshot.hud.roundTime = roundTimer_.asSeconds();
     snapshot.hud.gameOver  = (state_ == MatchState::WaitingForGameOverConfirmation);
     snapshot.hud.numPlayers = jugadores_.size();
@@ -284,24 +254,23 @@ RenderSnapshot Engine::makeRenderSnapshot()
     return snapshot;
 }
 
+//Funcion para transicionar del juego terminado a la pantalla de victoria. 
 void Engine::confirmGameOver() {
-    if (state_ != MatchState::WaitingForGameOverConfirmation)
-        return;
+    if (state_ != MatchState::WaitingForGameOverConfirmation) return;
 
-    // Award survival bonus to winner
     int winnerId = getWinnerId();
     
-    if (winnerId >= 0)
-        jugadores_[winnerId].anadirPuntos(300 + (int)roundTimer_.asSeconds() * 10);
+    //El ganador obtiene mas puntos a comparacion de los demas
+    if (winnerId >= 0) jugadores_[winnerId].anadirPuntos(300 + (int)roundTimer_.asSeconds() * 10);
 
-    // Save every player's result
+    //Se registran los puntajes de cada jugador en un ,txt
     for (const Player& p : jugadores_) {
         if (p.nombre() != ""){
                 ScoreManager::save(ScoreEntry{
                 p.nombre(),
                 p.puntaje(),
-                p.vida() > 0 ? 1 : 0,   // win: 1 if alive, 0 if not
-                p.muertes()               // add deaths counter to Player
+                p.vida() > 0 ? 1 : 0, //Si el jugador sobrevivio, se le da 1 victoria
+                p.muertes()
             });
         }
     }
@@ -312,7 +281,7 @@ void Engine::confirmGameOver() {
 std::string Engine::getWinnerName() const {
     for (const Player& p : jugadores_)
         if (p.vida() > 0) return p.nombre();
-    return "Nadie";   // round timer ran out
+    return "Nadie";   // Si se acaba el tiempo, o ningun jugador sobrevive, nadie gana
 }
 
 int Engine::getWinnerId() const {
@@ -325,124 +294,84 @@ void Engine::onPlayerMove(Evento &evento)
 {
     int playerId = evento.autor();
 
-    if (playerId < 0 || playerId >= jugadores_.size())
-        return;
+    //Se verifica que el jugador existe
+    if (playerId < 0 || playerId >= jugadores_.size()) return;
 
     Player& player = jugadores_[playerId];
 
     int dx = evento.data().mover.dx;
     int dy = evento.data().mover.dy;
 
+    //Se calcula la posicion de a donde se moveria el jugador
     Position oldPos = player.getPosition();
-
-    Position newPos{
-        oldPos.x + dx,
-        oldPos.y + dy
-    };
+    Position newPos{ oldPos.x + dx, oldPos.y + dy };
 
     const BoardCell cell = tablero_.getCell(newPos);
 
+    //Colision con entidades en el tablero
     for (const Occupant& occ : cell.occupants)
     {
         if (occ.type == EntityType::Enemy)
         {
-            danioPlayer(player.id());
-            return;
+            danioPlayer(player.id()); return;
         }
 
-    }
-
-    // Verificar si se puede caminar
-    if (!tablero_.isWalkable(newPos))
-    {
-        return;
-    }
-
-    // Mover occupant en tablero
-    bool moved = tablero_.moveOccupant(
-        oldPos,
-        newPos,
-        player.id()
-    );
-
-    if (!moved)
-    {
-        return;
-    }
-
-    // Actualizar posicion interna del jugador
-    player.setPosition(newPos.x, newPos.y);
-
-    for (const Occupant& occ : cell.occupants)
-    {
         if (occ.type == EntityType::Explosion)
         {
-            danioPlayer(
-                player.id()
-            );
-
-            break;
+            danioPlayer( player.id() ); return;
         }
 
         if (occ.type == EntityType::PowerUpItem)
         {
             pushEvento(Evento::playerPickPowerUp(player.id(), occ.entityId, newPos.x, newPos.y, powerUps_[occ.entityId].tipo()));
         }
+
     }
+
+    //Verificar si se puede caminar
+    if (!tablero_.isWalkable(newPos)) return;
+
+    //Mover jugador en tablero
+    bool moved = tablero_.moveOccupant( oldPos, newPos, player.id() );
+    if (!moved) { return; }
+
+    //Actualizar posicion interna del jugador
+    player.setPosition(newPos.x, newPos.y);
 }
 
 void Engine::danioPlayer(int playerId)
 {
-    if (playerId < 0 || playerId >= jugadores_.size())
-        return;
+    //Se verifica si el jugador existe o es invencible
+    if (playerId < 0 || playerId >= jugadores_.size()) return;
 
     Player& player = jugadores_[playerId];
 
-    if (player.esInvencible())
-        return;
+    if (player.esInvencible()) return;
 
     player.anadirMuerte();
     player.activarInvencibilidad();
 
     auto deathEvent = player.recibirDanio(player);
-    // Si el jugador muere, se procesa un evento de muerte. De lo contrario, se respawnea en su posicion inicial
-    if (deathEvent.has_value())
-    {
-        pushEvento(deathEvent.value());
-    }
+
+    // Si el jugador muere, se procesa un evento de muerte
+    if (deathEvent.has_value()) pushEvento(deathEvent.value());
+
     // Si el jugador no muere, se mueve a su posicion de spawn
     else
     {
         Position oldPos = player.getPosition();
-
-        Position spawnPos{
-            player.spawnPointX(),
-            player.spawnPointY()
-        };
+        Position spawnPos{ player.spawnPointX(), player.spawnPointY()};
         
-        tablero_.moveOccupant(
-            oldPos,
-            spawnPos,
-            player.id()
-        );
-
-        player.setPosition(
-            spawnPos.x,
-            spawnPos.y
-        );
+        tablero_.moveOccupant( oldPos, spawnPos, player.id() );
+        player.setPosition( spawnPos.x, spawnPos.y );
     }
-    
 }
 
 void Engine::onPlayerPlaceBomb(Evento& evento)
 {
     int playerId = evento.autor();
 
-    if (playerId < 0 ||
-        playerId >= static_cast<int>(jugadores_.size()))
-    {
-        return;
-    }
+    if (playerId < 0 || playerId >= static_cast<int>(jugadores_.size())) return;
 
     Player& player = jugadores_[playerId];
 
@@ -457,46 +386,22 @@ void Engine::onPlayerPlaceBomb(Evento& evento)
 void Engine::onBombExplode(Evento& evento)
 {
     int creador = evento.data().explosion.creador;
-
     jugadores_[creador].recuperarBomba();
-
     bombsSystem_.explodeBomb(evento);
 }
 
 void Engine::onTileDestroyed(Evento& evento)
 {
-    Position pos{
-        evento.posicionX(),
-        evento.posicionY()
-    };
+    Position pos{ evento.posicionX(), evento.posicionY()};
 
-    tablero_.setTerrain(
-        pos,
-        TileType::Floor
-    );
+    tablero_.setTerrain( pos, TileType::Floor );
 
+    //Generacion de PowerUps, con un 20% de probabilidad
     if (rand() % 100 < 20)
     {
-        PowerUpType tipo =
-            static_cast<PowerUpType>(
-                rand() % 3
-            );
-        
-        powerUps_.push_back(
-            PowerUp(
-                tipo,
-                pos.x,
-                pos.y
-            )
-        );
-
-        tablero_.addOccupant(
-            pos,
-            Occupant{
-                EntityType::PowerUpItem,
-                static_cast<int>(powerUps_.size() - 1)
-            }
-        );
+        PowerUpType tipo = static_cast<PowerUpType>( rand() % 3 );
+        powerUps_.push_back( PowerUp(tipo, pos.x, pos.y) );
+        tablero_.addOccupant( pos, Occupant{ EntityType::PowerUpItem, static_cast<int>(powerUps_.size() - 1)});
     }
 }
 
@@ -505,30 +410,21 @@ void Engine::onEnemyDeath(Evento& evento)
     int enemyId = evento.objetivo();
     int playerId = evento.autor();
 
+    //Se elimina el enemigo de enemyId, y se dan puntos al autor de la muerte
     enemiesSystem_.killEnemy(enemyId);
     jugadores_[playerId].anadirPuntos(100);
 
-    Position pos{
-        evento.posicionX(),
-        evento.posicionY()
-    };
-
-    tablero_.removeOccupant(
-        pos,
-        EntityType::Enemy,
-        enemyId
-    );
+    //Se remueve el enemigo
+    Position pos{ evento.posicionX(), evento.posicionY()};
+    tablero_.removeOccupant( pos, EntityType::Enemy, enemyId );
 }
 
 void Engine::onChainExplosion(Evento& evento)
 {
-    Position pos{
-        evento.posicionX(),
-        evento.posicionY()
-    };
-    for (auto it = bombas_.begin();
-     it != bombas_.end();
-     ++it)
+    Position pos{ evento.posicionX(), evento.posicionY() };
+    
+    //Se revisa si se colisiona con alguna bomba en la explosion
+    for (auto it = bombas_.begin(); it != bombas_.end(); ++it)
     {
         if (it->getId() == evento.autor())
         {
@@ -543,38 +439,24 @@ void Engine::onChainExplosion(Evento& evento)
             );
 
             bombas_.erase(it);
-
             break;
         }
     }
 }
 
-void Engine::onPlayerPickPowerUp(
-    Evento& evento
-)
+void Engine::onPlayerPickPowerUp(Evento& evento)
 {
     int playerId = evento.autor();
     int powerUpId = evento.objetivo();
 
-    PowerUpType tipo =
-        static_cast<PowerUpType>(
-            evento.data().powerUp.tipo
-        );
+    PowerUpType tipo = static_cast<PowerUpType>(evento.data().powerUp.tipo);
 
-    jugadores_[playerId]
-        .actualizarStat(
-            tipo,
-            1
-        );
+    //Todos los PowerUps aumentan en 1 el stat correspondiente del jugador
+    jugadores_[playerId].actualizarStat(tipo, 1);
 
-    tablero_.removeOccupant(
-        Position{
-            evento.posicionX(),
-            evento.posicionY()
-        },
-        EntityType::PowerUpItem,
-        powerUpId
-    );
+    //Se quita el PowerUp
+    Position pos = Position{ evento.posicionX(), evento.posicionY()};
+    tablero_.removeOccupant( pos, EntityType::PowerUpItem, powerUpId );
 }
 
 void Engine::handleInput(sf::Keyboard::Key key, int playerId)
@@ -584,7 +466,6 @@ void Engine::handleInput(sf::Keyboard::Key key, int playerId)
         {
             confirmGameOver();
         }
-        
         return;
     }
     else if (state_ == MatchState::Playing)
